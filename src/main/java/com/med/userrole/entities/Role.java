34a97b4f459e715337ae79ae3bc661a6280f386f
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-08 21:13:18 +0100

    RestController

--- src/main/java/com/med/userrole/entities/Role.java
+++ src/main/java/com/med/userrole/entities/Role.java
@@ -1,5 +1,6 @@
 package com.med.userrole.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,13 @@ import java.util.List;
 public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "DESCRIPTION")
     private String desc;
     @Column(unique = true, length = 20)
     private String roleName;
     @ManyToMany(fetch = FetchType.EAGER)
     //@JoinTable(name = "USERS_ROLES") // par default le type est join type
     @ToString.Exclude
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<User> users = new ArrayList<>();
 }
